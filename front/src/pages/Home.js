27d77f0d0b1a7aded9a2b15736eb665f8e834e93
Noee Pisano <noeepisanokoffel@gmail.com>
2023-01-02 14:11:35 +0100

    ajout components

--- front/src/pages/Home.js
+++ front/src/pages/Home.js
@@ -1,2 +1,20 @@
 // Lister les posts
 // Récent au + ancien
+
+import React from "react";
+import Card from "../components/Card";
+
+const Home = () => {
+    return (
+        <section className="main-container">
+            <h1>Les dernières publications</h1>
+            <div className="cards-container">
+                <Card />
+                <Button text="Anciennes publications" />
+            </div>
+
+        </section>
+    );
+};
+
+export default Home;
