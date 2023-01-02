db3f0d18187f0ffdd04fb9e0ac724d245c60cb31
Noee Pisano <noeepisanokoffel@gmail.com>
2023-01-02 12:56:12 +0100

    connect BDD + .env

---
+++ back/dataBase.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const result = dotenv.config();
+
+// Connexion à la base de données
+mongoose
+    .connect(
+        `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/?retryWrites=true&w=majority`,
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        }
+    )
+    .then(() => console.log("Connexion à MongoDB : OK"))
+    .catch(() => console.log("Connexion à MongoDB : FAIL"));
+
+module.exports = mongoose;
